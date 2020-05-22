45b768efcff1e09ad8fc8138124332ec1b0e3559
mlobo-dev <mlobo.dev@gmail.com>
2020-05-22 15:16:31 -0300

    teste deploy

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.07fc3dd74fec6988860b16396f4e4f3e.js"
+  "./precache-manifest.36e9fd6be9ee0746bd0e87850b9b1a55.js"
 );
 
 self.addEventListener('message', (event) => {
